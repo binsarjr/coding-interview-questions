fe7b4dfecc81f3673d50b39fc301ee6a9216ba04
Rajat Bhattacharjya <37776597+rajatbh21@users.noreply.github.com>
2021-10-02 21:31:01 +0530

    Added question and input/output patterns

--- topics/Arrays/threeSum.cpp
+++ topics/Arrays/threeSum.cpp
@@ -1,3 +1,15 @@
+//Question: Given an array and a value, find if there is a triplet in array whose sum is equal to the given value.
+//If there is such a triplet present in array, then print the triplet and return true. Else return false.
+
+/*Input: array = {12, 3, 4, 1, 6, 9}, sum = 24; 
+Output: 12, 3, 9 
+Explanation: There is a triplet (12, 3 and 9) present 
+in the array whose sum is 24. 
+Input: array = {1, 2, 3, 4, 5}, sum = 9 
+Output: 5, 3, 1 
+Explanation: There is a triplet (5, 3 and 1) present 
+in the array whose sum is 9.
+*/
 #include <bits/stdc++.h>
 using namespace std;
  
