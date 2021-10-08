eac60bfebcd10e39024f339fedd2a434f14c6be8
madhurmehta007 <77354138+madhurmehta007@users.noreply.github.com>
2021-10-08 18:58:05 +0530

    Create MainActivity.kt

---
+++ topics/Android/How to use Lottie Animation/Code/MainActivity.kt
@@ -0,0 +1,9 @@
+<com.airbnb.lottie.LottieAnimationView
+            android:id="@+id/animationView"
+            android:layout_width="match_parent"
+            android:layout_height="match_parent"
+            app:lottie_autoPlay="true"
+            app:lottie_url="https://assets6.lottiefiles.com/packages/lf20_mprazpz2.json"
+            //put your lottie url here
+            app:lottie_loop="false"
+            />
